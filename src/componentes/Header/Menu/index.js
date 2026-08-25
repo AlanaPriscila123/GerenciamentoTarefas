1ea99b65cc49c167efe9cee85e1333db4eca639c
AlanaPriscila123 <alana.marques.ramos@escola.pr.gov.br>
2026-08-25 12:16:53 -0300

    Add files via upload

--- src/componentes/Header/Menu/index.js
+++ src/componentes/Header/Menu/index.js
@@ -1,15 +1,8 @@
-import { Link } from "react-router-dom"
 function Menu(){
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">home</Link></li>
-                <li><Link to="/contato">Contato</Link></li>
-                <li><Link to="/Sobre">Sobre</Link></li>
-
-            </ul>
-       
-        </nav>
+        <>
+        Menu
+        </>
     )
 }
 
